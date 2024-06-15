e8bfaa105e5e13c1366123f7d3d79f0260c59072
jeylanab <jeylantesi@gmail.com>
2024-06-15 15:28:06 +0530

    added utility folder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./index.html", "./src/**/*.{js,jsx}"];
+export const mode = "jit";
+export const theme = {
+  extend: {},
+  screens: {
+    xs: "480px",
+    ss: "620px",
+    sm: "768px",
+    md: "1060px",
+    lg: "1200px",
+    xl: "1700px",
+  },
+};
+export const plugins = [];
