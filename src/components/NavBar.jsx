938e433652a3d27e58f08a92a8f73e11e8e9c7b4
jeylanab <jeylantesi@gmail.com>
2024-07-06 12:17:59 +0530

    added border of input line

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,8 +11,8 @@ const NavBar = () => {
       <div className=' flex justify-between items-center m-5'>
         {/* menus div */}
         <div className='  flex justify-center items-center '>
-          <ul className= 'flex  items-center justify-center  p-0'>{navLinks.map((item, index) => (
-            <li className='mx-2' key={index}>{item.title}</li>
+          <ul className= 'flex  items-center justify-center  p-0 m-0'>{navLinks.map((item, index) => (
+            <li className='mx-3' key={index}>{item.title}</li>
             
             ))}
           </ul>
@@ -24,10 +24,10 @@ const NavBar = () => {
         </div>
         {/* icon div */}
         <div className='p-0 nav-icon flex justify-center items-center'>
-           <div><input type="text" /></div>
-           <img src={favorite} alt="" />
-           <img src={cart} alt="" />
-           <img src={profile} alt="" />
+           <div><input className='mx-2 rounded-md p-1 border border-gray-300 focus:border-orange-500 outline-none ' type="text" placeholder='Search for Interest' /></div>
+           <img className='mx-2' src={favorite} alt="" />
+           <img className='mx-2' src={cart} alt="" />
+           <img className='mx-2' src={profile} alt="" />
         </div>
          
       </div>
