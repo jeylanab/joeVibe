5cf62218dbe3a142a90540fe8f9617e1812e0b0f
jeylanab <jeylantesi@gmail.com>
2024-07-06 11:25:04 +0530

    added asset

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { navLinks } from '../constants/data'
 const NavBar = () => {
   return (
     <div>
-      
-    
+      <div className=' flex justify-between items-center'></div>
+       
     </div>
   )
 }
