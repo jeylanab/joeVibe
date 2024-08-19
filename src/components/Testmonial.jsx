bfd2c8768c4420bf503615015d3fad749a354cb8
jeylanab <jeylantesi@gmail.com>
2024-08-19 09:36:29 +0530

    added font on to the react app

--- src/components/Testmonial.jsx
+++ src/components/Testmonial.jsx
@@ -11,17 +11,17 @@ const Testmonial = () => {
               <div className=' side-card '>
                   <h1 className=' text-center text-2xl font-medium p-3'>Join <span className=' text-shadow text-4xl font-bold'>32k+</span>  style enthusiasts growing their brands with JoeVibe</h1>
                   <div className=' flex justify-center items-center'>
-                    <img className='m-2' src={lg1} alt="" />
-                    <img className='m-2' src={lg2} alt="" />
-                    <img className='m-2' src={lg3} alt="" />
-                    <img className='m-2' src={lg4} alt="" />
+                    <img className='m-2' src={lg1} alt="brand-one" />
+                    <img className='m-2' src={lg2} alt="brand-two" />
+                    <img className='m-2' src={lg3} alt="brand-three" />
+                    <img className='m-2' src={lg4} alt="brand-four" />
             
                   </div>
           
               </div>
           </div>
           <div className=' test-background'>
-              <h1 className='pt-[100px]'>What we do</h1>
+              <h1 className='pt-[100px] title-color text-center text-4xl font-semibold'>What we do</h1>
         
         
              
